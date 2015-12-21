957d78722174890b6f3d260c3af636cd0c10949b
Aaron Cotter <acotter@tfs.ca>
2015-12-21 08:26:51 -0500

    Please enter the commit message for your changes. Lines starting

--- hashtable.c
+++ hashtable.c
@@ -42,4 +42,27 @@ typedef struct node{
 }node;
 
 node *table[CAPACITY];
-
+/*
+Hash Tables have a key and a value; we have an associative array
+Key: Paul
+Value: phone # 
+The hash functions gets a key as inputs, and returns a value (index in an array). 
+The hash function should return the same value if the index is the same 
+Using seperate chaining is the preferable method to deal with conflicts
+O(n/k) where k is the size of the array (k times faster than a single linked list assuming an even distribution)
+*/
+Potential Hashing Functions:
+DJB2;
+unsigned int hashFunction(hash_table *hashtable, char *str){
+    unsigned long hash = 5381;
+    for(; *str; str++)
+        hash += hash << 5 + *str;
+    return hash%(hashtable->size);
+}
+Lose Lose
+unsigned int hashFunction(hash_table *hashtable, char *str){
+    unsigned int hash = 0;
+    for(; *str; str++)
+        hash+= *str;
+    return hash%(hashtable->size);
+}
