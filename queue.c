8d7004b369ec1e2a684110094182c6d34b756e15
Aaron Cotter <acotter@tfs.ca>
2015-12-21 12:35:25 -0500

    queue implementation with array

--- queue.c
+++ queue.c
@@ -4,6 +4,12 @@
 //Implementation 1: Using an array
 const int CAPACITY = 100;
 
+/***********************
+Time Complexity Summary:
+Enqueuing and Dequeing are O(1) operations due to randome access of the array
+Size of the elements array is fixed, thus inefficient use of space.
+***********************/
+
 typedef struct{
     int head; //index of element at the head
     int length;
@@ -21,27 +27,20 @@ queue *queueInit(int value){
 }
 //Dequeuing
 void deque(queue *new){
-    if(new->length <= 0) return;
-    new->head--;  //= (new->head+1)%CAPACITY;
+    if(new->length <= 0)
+        return;
+    new->elements[new->head] = 0;
+    new->head = (new->head + 1)%CAPACITY;
     new->length--;
     return;
 }
 //Enqueuing
 void enque(queue *new, int n){
-    //shift everything down one position and insert
-    if(new->length+1 > CAPACITY) return;
-    if(new->length > 1){
-        for(int i = new->length-2; i <= 0; i--){
-            new->elements[i+1] = new->elements[i];
-        }
-        new->elements[0] = n;
-    }
-    if(new->length == 1){
-        new->elements[new->length] = new->elements[0];
-        new->elements[0] = n;
-    }
+    if(new->length > CAPACITY)
+        return;
+    new->elements[(new->head+new->length)%CAPACITY] = n;
     new->length++;
-    new->head++;
+    return;
 }
 //Returning Size
 int queueSize(queue *new){
@@ -56,28 +55,37 @@ int queueEmpty(queue *new){
 }
 //Contains Element
 int queueContains(queue *new, int n){
-    for(int i = 0; i < new->length; i++){ //linear search
+    for(int i = new->head; i < new->head + new->length; i++){ //linear search
         if(new->elements[i] == n)
             return 1;
     }
     return 0; //not found
 }
+//Free the memory
 void queueClear(queue *new){
-    free(new->elements);
     free(new);
     return;
 }
 void queuePrint(queue *new){
-    for(int i = 0; i < new->length; i++)
+    for(int i = new->head; i < new->head + new->length; i++)
         printf("%d ", new->elements[i]);
     printf("\n");
 }
 int main(){
     queue *new = queueInit(5);
     enque(new,10);
-    //enque(new,15);
-    //enque(new,20);
-    //enque(new,25);
+    enque(new,15);
+    enque(new,20);
+    enque(new,25);
     queuePrint(new);
+    deque(new);
+    deque(new);
+    deque(new);
+    enque(new,1000);
+    printf("size is %d\n", queueSize(new));
+    queuePrint(new);
+    printf("contains: %d\n",queueContains(new,25));
+    printf("contains: %d\n",queueContains(new,2));
+    queueClear(new);
     return 0;
 }
