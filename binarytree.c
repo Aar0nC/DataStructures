86ffe7328c81802143df7ddaf10cb46d78b7d4ad
Aaron Cotter <acotter@tfs.ca>
2015-12-22 13:22:58 -0500

    optimized binaryFree

--- binarytree.c
+++ binarytree.c
@@ -1,21 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/***********
-Binary search tree: a tree where all nodes to the left precede the root 
-in some ordered set, and the nodes to the right proceed the root in the same ordered set. 
-Each branch is of degree two. 
-The longest path from the root to a leaf is the depth of the tree
-Binary Search trees naturally lend themselves to recursive methods
- 
-Operations to code: 
-1. Insertion / Creation
+/*Remaining:
 2. Deletion
-3. Printing
 4. Searching
 5. Traversing
-6. Freeing 
-************/
+*/
 
 int compare(const void *_a, const void *_b){
     int *a = (int *)_a;
@@ -28,20 +17,6 @@ typedef struct node{
     struct node *left;
     struct node *right;
 }node;
-
-/*node *insert(node *root, int data){
-    if(root = NULL){
-        root = malloc(sizeof(node));
-        root->value = data;
-        root->left = NULL;
-        root->right = NULL;
-    }
-    else if(data < r->value)
-        root->left = buildBinary(r->left, data);
-    else
-        root->right = insert(r>right,data);
-    return root
-}*/
 
 node *buildBinaryTree(int *array, int n){
     if(n == 0)
@@ -56,20 +31,11 @@ node *buildBinaryTree(int *array, int n){
 void binaryPrint(node *root){
     if(root == NULL)
         return;
-    printf("%d\n", root->value);
+    printf("%d ", root->value);
     binaryPrint(root->left);
     binaryPrint(root->right);
 }
 void binaryFree(node *root){
-    //again, use recursion
-    /*if(root == NULL)
-        return;
-    node *crawler = root;
-    node *rootL = root->left;
-    node *rootR = root->right;
-    free(crawler);
-    binaryFree(rootL);
-    binaryFree(rootR);*/
     if(root == NULL)
         return;
     binaryFree(root->left);
@@ -77,11 +43,12 @@ void binaryFree(node *root){
     free(root);
 }
 int main(){
-    int array[] = {9,4,2,6,12,15,17};
+    int array[] = {1,2,5,7,10,11,12,14,16,20};
     int n = sizeof(array)/sizeof(array[0]);
     qsort(array,n,sizeof(int), compare);
     node *root = buildBinaryTree(array,n);
     binaryPrint(root);
+    printf("\n");
     binaryFree(root);
     return 0;
 }
